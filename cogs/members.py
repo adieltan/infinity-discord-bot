fe0e69f54e1d4573b86e54f96068a630f6a68538
adieltan <75818908+adieltan@users.noreply.github.com>
2021-06-30 21:41:25 +0800

    mutual

--- cogs/members.py
+++ cogs/members.py
@@ -82,7 +82,7 @@ class MembersCog(commands.Cog, name='Members'):
     async def mutual(self, ctx, user:discord.User):
         """Returns the servers that are shared with the user."""
         servers = '\n'.join([f"`{guild.id}` {guild.name}" for guild in user.mutual_guilds])
-        embed=discord.Embed(title="Mutual servers", description=servers, color=discord.Color.random())
+        embed=discord.Embed(title="Mutual servers", description=servers, color=discord.Color.random(), timestamp=datetime.datetime.utcnow())
         embed.set_author(name=user.name, icon_url=user.avatar_url)
         embed.set_footer(text=f"{len(user.mutual_guilds)} servers")
         await ctx.reply(embed=embed)

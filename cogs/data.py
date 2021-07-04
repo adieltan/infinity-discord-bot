f780a7438d4b78c0c30dd58f70ae7aad0e7c8e56
adieltan <75818908+adieltan@users.noreply.github.com>
2021-07-04 11:32:19 +0800

    error

--- cogs/data.py
+++ cogs/data.py
@@ -18,37 +18,34 @@ class DataCog(commands.Cog, name='Data'):
         """Plots the xp of the server's amari data in a chart."""
         rank = []
         xp = []
-        try:
-            async with aiohttp.ClientSession() as cs:
-                async with cs.get(url=f"https://lb.amaribot.com/index.php?gID={ctx.guild.id}") as data:
-                    soup = BeautifulSoup(data.content, "html.parser")
-            doc = lh.fromstring(soup.content)
-            tr_elements = doc.xpath('//tr')
-            for t in tr_elements:
-                rawrank=int(t[0].text_content())
-                rawxp = int(t[2].text_content())
-                if rawxp < 0:
-                    pass
-                else:
-                    rank.append(rawrank)
-                    xp.append(rawxp)
-            plt.plot(rank, xp, label = "xp")
-            plt.xlabel('Rank')
-            plt.ylabel('XP')
-            plt.title(f"{ctx.guild.name}'s Amari Xp")
-            buf = io.BytesIO()
-            plt.savefig(buf, format='png')
-            buf.seek(0)
-            f = discord.File(fp=buf, filename="image.png")
-            embed=discord.Embed(title=f"{ctx.guild.name}'s Amari Xp")
-            embed.set_image(url="attachment://image.png")
-            embed.timestamp=datetime.datetime.utcnow()
-            await ctx.reply(file=f, embed=embed)
-            plt.close()
-            buf.close()
-            await cs.close()
-        except:
-            await ctx.reply("Error.")
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(url=f"https://lb.amaribot.com/index.php?gID={ctx.guild.id}") as data:
+                soup = BeautifulSoup(data.content, "html.parser")
+        doc = lh.fromstring(soup.content)
+        tr_elements = doc.xpath('//tr')
+        for t in tr_elements:
+            rawrank=int(t[0].text_content())
+            rawxp = int(t[2].text_content())
+            if rawxp < 0:
+                pass
+            else:
+                rank.append(rawrank)
+                xp.append(rawxp)
+        plt.plot(rank, xp, label = "xp")
+        plt.xlabel('Rank')
+        plt.ylabel('XP')
+        plt.title(f"{ctx.guild.name}'s Amari Xp")
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        f = discord.File(fp=buf, filename="image.png")
+        embed=discord.Embed(title=f"{ctx.guild.name}'s Amari Xp")
+        embed.set_image(url="attachment://image.png")
+        embed.timestamp=datetime.datetime.utcnow()
+        await ctx.reply(file=f, embed=embed)
+        plt.close()
+        buf.close()
+        await cs.close()
 
 def setup(bot):
     bot.add_cog(DataCog(bot))

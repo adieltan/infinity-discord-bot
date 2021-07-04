ba6ee13aa19d46d219b97abd0c381226d1b8476d
adieltan <75818908+adieltan@users.noreply.github.com>
2021-07-04 11:29:32 +0800

    Amari graph

--- cogs/data.py
+++ cogs/data.py
@@ -5,14 +5,50 @@ import matplotlib.pyplot as plt
 try:from dotenv import load_dotenv
 except:pass
 
+import lxml.html as lh
+from bs4 import BeautifulSoup
+
 class DataCog(commands.Cog, name='Data'):
     """*Data from websites or api*"""
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(name="youtube", aliases=['yt'])
-    async def youtube(self, ctx, *, search:str):
-        pass
+    @commands.command(name="amari", aliases=['amarigraph'])
+    async def amari(self, ctx):
+        """Plots the xp of the server's amari data in a chart."""
+        rank = []
+        xp = []
+        try:
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get(url=f"https://lb.amaribot.com/index.php?gID={ctx.guild.id}") as data:
+                    soup = BeautifulSoup(data.content, "html.parser")
+            doc = lh.fromstring(soup.content)
+            tr_elements = doc.xpath('//tr')
+            for t in tr_elements:
+                rawrank=int(t[0].text_content())
+                rawxp = int(t[2].text_content())
+                if rawxp < 0:
+                    pass
+                else:
+                    rank.append(rawrank)
+                    xp.append(rawxp)
+            plt.plot(rank, xp, label = "xp")
+            plt.xlabel('Rank')
+            plt.ylabel('XP')
+            plt.title(f"{ctx.guild.name}'s Amari Xp")
+            buf = io.BytesIO()
+            plt.savefig(buf, format='png')
+            buf.seek(0)
+            f = discord.File(fp=buf, filename="image.png")
+            embed=discord.Embed(title=f"{ctx.guild.name}'s Amari Xp")
+            embed.set_image(url="attachment://image.png")
+            embed.timestamp=datetime.datetime.utcnow()
+            await ctx.reply(file=f, embed=embed)
+            plt.close()
+            buf.close()
+            await cs.close()
+        except:
+            await ctx.reply("Error.")
 
 def setup(bot):
     bot.add_cog(DataCog(bot))

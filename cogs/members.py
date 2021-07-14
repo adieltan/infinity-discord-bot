5d24f7dcf4c678d90483116b251e019e9e964076
adieltan <75818908+adieltan@users.noreply.github.com>
2021-07-14 20:34:07 +0800

    lol wtf i went and printed "result" instead of the result

--- cogs/members.py
+++ cogs/members.py
@@ -27,7 +27,7 @@ class MembersCog(commands.Cog, name='Members'):
             result = await self.bot.http.request(
                 Route("GET", f"/users/{member.id}")
             )
-            await ctx.send('result')
+            await ctx.send(f'{result}')
             bio = result['bio']
             embed.add_field(name="Bio", value=f"{bio}", inline=False)
         except Exception as e:

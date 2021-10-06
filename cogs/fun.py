f5836355ac305253e21afca08762d8e0e1f6e3f1
adieltan <75818908+adieltan@users.noreply.github.com>
2021-10-06 16:26:59 +0800

    .

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,6 +9,7 @@ class FunCog(commands.Cog, name='Fun'):
     """🥳 Fun / minigame commands."""
     def __init__(self, bot):
         self.bot = bot
+        self.ongoing_mm_games = dict()
     
     @commands.command(name= 'roll', aliases=['dice', 'throw'])
     @commands.cooldown(1,5)
@@ -558,6 +559,93 @@ class FunCog(commands.Cog, name='Fun'):
 
         await choose.reply(f"{ctx.author.mention}",embed=embed)
     
+    @commands.command(name='messagemania', aliases=['mm'])
+    @commands.has_permissions(manage_messages=True)
+    async def messagemania(self, ctx, seconds:int=None):
+        """Message Mania Minigame."""
+        timer = '<a:timer:890234490100793404>'
+        if ctx.channel.id in self.ongoing_mm_games.keys():
+            await ctx.reply(f"There is an ongoing game in this channel.")
+            return
+        if seconds is None:
+            seconds = 390
+        overwrite = ctx.channel.overwrites_for(ctx.guild.default_role)
+        overwrite.send_messages=True
+        await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+        message = await ctx.send(embed=discord.Embed(description=f"<a:verified:876075132114829342> {ctx.channel.mention} Unlocked\nChannel will be locked soon.\n\n__Commands:__\n`mmp`: Purges 10 messages from the channel.\n`mmu`: Purges messages from a random user.\n`mmm`: Mutes a user from talking for 30 seconds.", colour=discord.Color.green()))
+        self.ongoing_mm_games[ctx.channel.id] = message.created_at
+        timestamp = round(message.created_at.replace(tzinfo=datetime.timezone.utc).timestamp())
+        await message.edit(embed=discord.Embed(description=f"<a:verified:876075132114829342> {ctx.channel.mention} Unlocked\nChannel will be locked at <t:{timestamp+seconds}:T> <t:{timestamp+seconds}:R>.\n\n__Commands:__\n`mmp`: Purges 10 messages from the channel.\n`mmu`: Purges messages from a random user.\n`mmm`: Mutes a user from talking for 30 seconds.", colour=discord.Color.green()))
+        await asyncio.sleep(seconds)
+        overwrite.send_messages=False
+        await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+        await ctx.send(embed=discord.Embed(description=f"<a:verified:876075132114829342> {ctx.channel.mention} Locked", colour=discord.Color.red()))
+        messages = await ctx.channel.history(limit=None, after=message.created_at).flatten()
+        messages = [x.author.id for x in messages if x.author.bot is False]
+        counter=collections.Counter(messages)
+        winners = '\n'.join(f"<medal here>  <@{x[0]}>: {x[1]} messages" for x in counter.most_common(5))
+        winners = winners.replace('<medal here>', '🥇', 1).replace('<medal here>', '🥈', 1).replace('<medal here>', '🥉', 1).replace('<medal here>', '🏅', 1).replace('<medal here>', '🏅', 1)
+        embed = discord.Embed(title="Message Mania", description=f"**__Winners__**\n{winners}", color=discord.Color.gold()).set_thumbnail(url="https://images-ext-1.discordapp.net/external/LMTQPkVKqF0jESGgD5djPe1ROAUCybuofm-ismCdBUs/https/media.discordapp.net/attachments/841654825456107533/890903767845834762/MM.png")
+        try:
+            await ctx.reply(embed=embed)
+        except:
+            await ctx.send(embed=embed)
+        del self.ongoing_mm_games[ctx.channel.id]
+
+    @commands.command(name='mmp', hidden=True)
+    @commands.cooldown(1,60, commands.BucketType.user)
+    @server([841654825456107530])
+    async def messagemaniammp(self, ctx):
+        if ctx.channel.id not in self.ongoing_mm_games.keys():
+            return
+        def pinc(msg):
+            if msg.pinned or msg.id == ctx.message.id or msg.author.bot is True:
+                return False
+            else:
+                return True
+        try:    
+            await ctx.channel.purge(limit=10, check=pinc, after=self.ongoing_mm_games[ctx.channel.id])
+            await ctx.message.add_reaction('<a:verified:876075132114829342>')
+        except:
+            pass
+
+    @commands.command(name='mmu', hidden=True)
+    @commands.cooldown(1,180, commands.BucketType.channel)
+    @server([841654825456107530])
+    async def messagemaniammu(self, ctx):
+        if ctx.channel.id not in self.ongoing_mm_games.keys():
+            return
+
+        messages = await ctx.channel.history(after=self.ongoing_mm_games[ctx.channel.id]).flatten()
+        user = random.choice([set([x.author.id for x in messages if x.author.bot is not True])])
+
+        def pinc(msg):
+            if msg.pinned or msg.id == ctx.message.id or msg.author.id != next(iter(user)):
+                return False
+            else:
+                return True
+        try:
+            await ctx.channel.purge(limit=100, check=pinc, after=self.ongoing_mm_games[ctx.channel.id])
+            await ctx.message.add_reaction('<a:verified:876075132114829342>')
+        except:
+            pass
+
+    @commands.command(name='mmm', hidden=True)
+    @commands.cooldown(1,120, commands.BucketType.channel)
+    @server([841654825456107530])
+    async def messagemaniammm(self, ctx):
+        if ctx.channel.id not in self.ongoing_mm_games.keys():
+            return
+        try:
+            messages = await ctx.channel.history(after=self.ongoing_mm_games[ctx.channel.id]).flatten()
+            user = random.choice([set([x.author for x in messages if x.author.bot is not True])])
+            await ctx.channel.set_permissions(next(iter(user)), send_messages=False)
+            await ctx.send(f"{next(iter(user)).mention} muted for 30s.")
+            await asyncio.sleep(30)
+            await ctx.channel.set_permissions(user.pop(), overwrite=None)
+            await ctx.message.add_reaction('<a:verified:876075132114829342>')
+        except:
+            pass
 
 def setup(bot):
     bot.add_cog(FunCog(bot))

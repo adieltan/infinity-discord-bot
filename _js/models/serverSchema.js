65865f8380295dcf49e8d7e6be1bef187ddd107f
Adiel Tan <75818908+adieltan@users.noreply.github.com>
2021-12-06 09:26:09 +0800

    js branch move

--- _js/models/serverSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("server", new mongoose.Schema({
-    _id: { type: String },
-    prefix: {type: String, default: '='},
-    autoresponse: {type: Object, default: {}},
-    autoreact: {type: Object, default: {}}
-}));
